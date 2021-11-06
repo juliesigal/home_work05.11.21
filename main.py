35c362eb0e2974234c4e4b8aa2f220ce04d2b137
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-06 21:32:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ def isDictKeysAlpha(d):
             continue
     return True
   
+**********targil 2:
+  
+def popByValue(dict):
+    etgar = []
+    for k,v in list(dict.items()):
+        if type(dict[k]) == int:
+            etgar.append(dict[k])
+            dict.pop(k)
+    return etgar  
+  
  **********targil 3:
   
  def compareDict(d1,d2):
@@ -14,3 +24,14 @@ def isDictKeysAlpha(d):
         return True
     else:
         return False 
+
+ **********targil 4:     
+      
+      
+ def newDict(d1,d2):
+    dict = {}
+    if len(list1) != len(list2):
+        return None
+    for i in range(len(list1)):
+        dict[list1[i]] = list2[i]
+    return dict     
