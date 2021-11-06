d8b198881f2090b9a991c40985cd04ab76a113c7
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-06 19:47:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+**********targil 1:
+  
+def isDictKeysAlpha(d):
+    for k in dict_city.keys():
+        if k.isalpha:
+            continue
+    return True
+  
+ **********targil 3:
+  
+ def compareDict(d1,d2):
+    if d1 == d2:
+        print(d1,d2)
+        return True
+    else:
+        return False 
